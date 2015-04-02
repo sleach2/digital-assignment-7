b695597cad38d5897a16306a46df2b1c48ed03d8
sleach2 <sleach2@gmu.edu>
2015-04-02 18:04:18 -0400

    w

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
+        game.load.image('bkg','assets/veins.png');
     }
     
+    var bkg;
     
     function create() {
-
+        bkg=game.add.sprite(0,0,'bkg');
     }
     
     function update() {
