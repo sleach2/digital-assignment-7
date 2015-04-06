9265146c575ed9db5901c0c35961f8fe8116b17e
sleach2 <sleach2@gmu.edu>
2015-04-06 11:09:07 -0400

    jm

--- js/main.js
+++ js/main.js
@@ -5,15 +5,36 @@ window.onload = function() {
     
     function preload() {
         game.load.image('bkg','assets/veins3.png');
+        game.load.image('ship', 'assets/ship.png');
     }
     
     var bkg;
+    var player;
+    var music;
     
     function create() {
+        //music=game.add.audio('bks');
+        //music.play('',0,0.5,true);
+
+        //game.world.setBounds(0,0,2000,2000);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
         bkg=game.add.sprite(0,0,'bkg');
+
+        player = game.add.sprite(0, game.world.height-130, 'player');
+        game.physics.arcade.enable(player);  
+        player.body.collideWorldBounds = true;
     }
     
     function update() {
-        
+        //game.physics.arcade.collide(player, platforms);
+        player.body.velocity.x=0;
+        if (cursors.left.isDown){
+            player.body.velocity.x = -250;
+            //player.animations.play('left');
+        }else if (cursors.right.isDown){
+            player.body.velocity.x = 250;
+            //player.animations.play('right');
+        }
     }
 };
