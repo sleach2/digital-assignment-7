243eea48fe962f103a4da24e057ad2d220038509
sleach2 <sleach2@gmu.edu>
2015-04-06 17:33:04 -0400

    jhg

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ window.onload = function() {
         blackenemy.setAll('checkWorldBounds', true);
 
         timer = game.time.create(false);
-        timer.loop(1500, spawn(), this);
+        timer.loop(500, spawn(), this);
         timer.start();
     }
 
